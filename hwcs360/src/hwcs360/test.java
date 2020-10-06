f810171c6beba7fe1e15c0ebb79c8b9c479585ee
allenlin <allenlin@10.0.0.190>
2020-10-06 00:39:45 -0500

    First change to display hello message

--- hwcs360/src/hwcs360/test.java
+++ hwcs360/src/hwcs360/test.java
@@ -1,5 +1,14 @@
 package hwcs360;
 
 public class test {
-
+	public static void main(String[] args) {
+		displayGreeting();
+		
+	}
+	
+	private static void displayGreeting() {
+		System.out.println("Hello");
+		
+	}
 }
+
